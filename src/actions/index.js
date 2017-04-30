893e3fb417dfb37e23a2a367315fafe4e97d1ecf
Aori Nevo <aori.nevo@bloomingdales.com>
2017-04-30 11:24:25 -0400

    Wire up SearchBar to OpenWeatherMap Api.

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'be7e6bdcd53dbc8d087038b8ff34be9f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather( city ){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
